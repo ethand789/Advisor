9c4771ae0bd46910f2fbbf36b316e15b5cb0845b
ethan <ethand789@gmail.com>
2023-12-20 15:16:49 -0500

    it still works on a new machine and version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+mod jokes;
+
 use std::io;
 
 fn main() {
     let mut input = String::new();
     let exit_phrase = String::from("exit");
     let mut command_list:Vec<String> = Vec::new();
+    command_list.push(String::from(""));
 
-    while(String::from(command_list.last().unwrap()) != exit_phrase){ //exiting while loop terminates program
+    while (String::from(command_list.last().unwrap()) != exit_phrase){ //exiting while loop terminates program
 
-        println!("please enter your next command (enter '{}' to exit)", exit_phrase); //get input from user
+        println!("\nplease enter your next command (enter '{}' to exit)", exit_phrase); //get input from user
         io::stdin()
             .read_line(&mut input)
             .expect("Could not read command");
@@ -16,10 +19,10 @@ fn main() {
 
         input = String::new(); //clear input buffer
 
-       for x in command_list.iter(){ //print all commands in command_list
-           println!("{}",x);
-       }
-        print!("\n\n last command: {}",command_list.last().unwrap()); //print last command in command_list
+     //  for x in command_list.iter(){ //print all commands in command_list
+     //      println!("{}",x);
+     //  }
+        print!("\nlast command: {} ",command_list.last().unwrap()); //print last command in command_list
     }
 }
 
